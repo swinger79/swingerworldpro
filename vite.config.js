b1a31fc0c2a656c8f1c3593706049f537682def4
swinger79 <dev@swinguerworldpro.com>
2026-01-07 12:07:34 +0100

    Fix allowedHosts to accept all domains

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 8080,
-    strictPort: false
+    port: 8080
   },
   preview: {
     host: '0.0.0.0',
     port: 8080,
-    strictPort: false,
     allowedHosts: 'all'
   },
   build: {

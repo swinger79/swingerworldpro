7b45e1ba03f2ce89a8d8a2885a709e20c38df106
swinger79 <dev@swinguerworldpro.com>
2026-01-07 10:45:13 +0100

    Fix Railway server config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,9 +7,12 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT || 3000
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser'
+    minify: 'esbuild',
+    outDir: 'dist'
   }
-});
+})
